b73ae88e9362cae399a6f5f55da752d4ee88af63
Alex M <xbestyan@hotmail.com>
2020-05-02 22:27:19 +0300

    carousel

--- DFStore.WebSite/Models/Product.cs
+++ DFStore.WebSite/Models/Product.cs
@@ -16,6 +16,8 @@ namespace DFStore.WebSite.Models
         public string Image { get; set; }
         public string Url { get; set; }
         public string Title { get; set; }
+        public string Price { get; set; }
+
         public string Description { get; set; }
         public int[] Ratings { get; set; }
 
